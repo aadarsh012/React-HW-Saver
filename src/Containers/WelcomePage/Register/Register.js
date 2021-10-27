b66abf14cd4e74588d26cbdbd404aed7b449d0f3
aadarsh012 <aadarsh012@gmail.com>
2021-10-27 11:05:26 +0530

    Modified Form

--- src/Containers/WelcomePage/Register/Register.js
+++ src/Containers/WelcomePage/Register/Register.js
@@ -1,6 +1,6 @@
 import "./Register.css";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import Input from "../../../UI/Input/Input";
 
 const Register = (props) => {
@@ -8,41 +8,144 @@ const Register = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [isSignup, setIsSignup] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    setEmail("");
+    setUsername("");
+    setPassword("");
+    setConfirmPassword("");
+  }, [isSignup]);
+
+  const switcAuthHandler = () => {
+    setIsSignup(!isSignup);
+  };
+
+  const registerHandler = async (event) => {
+    event.preventDefault();
+
+    if (!username || !email || !password || !confirmPassword) {
+      setError("Please Provide Valid Credentials.");
+      setTimeout(() => {
+        setError("");
+      }, 3000);
+    } else if (password.length < 6) {
+      setError("Password should be of atleast 6 characters.");
+      setTimeout(() => {
+        setError("");
+      }, 3000);
+    } else if (password !== confirmPassword) {
+      setError("Passwords do not match.");
+      setTimeout(() => {
+        setError("");
+      }, 3000);
+    } else {
+      try {
+        const response = await axios.post(
+          "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAt7v3kUYonmcmbxZvIgV9MIDmN4G8xq_s",
+          { email, password, returnSecureToken: true }
+        );
+        localStorage.setItem("token", response.data.idToken);
+      } catch (error) {
+        setError("Email Exists. Please Login.");
+        setTimeout(() => {
+          setError("");
+        }, 3000);
+      }
+    }
+  };
+
+  const loginHandler = async (event) => {
+    event.preventDefault();
+
+    if (!email || !password) {
+      setError("Please Provide Valid Credentials.");
+      setTimeout(() => {
+        setError("");
+      }, 3000);
+    } else {
+      try {
+        const response = await axios.post(
+          "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAt7v3kUYonmcmbxZvIgV9MIDmN4G8xq_s",
+          { email, password, returnSecureToken: true }
+        );
+        localStorage.setItem("token", response.data.idToken);
+      } catch (error) {
+        setError("Invalid Email or Password.");
+        setTimeout(() => {
+          setError("");
+        }, 3000);
+      }
+    }
+  };
+
+  const authForm = isSignup ? (
+    <form onSubmit={registerHandler} className="registerForm">
+      <div className="error">{error && <span>*{error}</span>}</div>
+      <h2>Register</h2>
+      <Input
+        label="Username"
+        type="text"
+        changed={(event) => setUsername(event.target.value)}
+        value={username}
+      />
+      <Input
+        label="Email"
+        type="email"
+        changed={(event) => setEmail(event.target.value)}
+        value={email}
+      />
+      <Input
+        label="Password"
+        type="password"
+        changed={(event) => setPassword(event.target.value)}
+        value={password}
+      />
+      <Input
+        label="Confirm Password"
+        type="password"
+        changed={(event) => setConfirmPassword(event.target.value)}
+        value={confirmPassword}
+      />
+      <button type="submit">REGISTER</button>
+      <span>
+        Already have an account?
+        <span className="switchAuth" onClick={switcAuthHandler}>
+          Login.
+        </span>
+      </span>
+    </form>
+  ) : (
+    <form onSubmit={loginHandler} className="loginForm">
+      <div className="error">{error && <span>*{error}</span>}</div>
+      <h2>Login</h2>
+      <Input
+        label="Email"
+        type="email"
+        changed={(event) => setEmail(event.target.value)}
+        value={email}
+      />
+      <Input
+        label="Password"
+        type="password"
+        changed={(event) => setPassword(event.target.value)}
+        value={password}
+      />
+      <button type="submit">Login</button>
+      <span>
+        New User?
+        <span className="switchAuth" onClick={switcAuthHandler}>
+          Register.
+        </span>
+      </span>
+    </form>
+  );
 
   return (
     <div className="Register">
       <img src="https://source.unsplash.com/1600x900/?space" alt="Space" />
-      <form>
-        <h2>Register</h2>
-        <Input
-          label="Username"
-          type="text"
-          changed={(event) => setUsername(event.target.value)}
-          value={username}
-        />
-        <Input
-          label="Email"
-          type="email"
-          changed={(event) => setEmail(event.target.value)}
-          value={email}
-        />
-        <Input
-          label="Password"
-          type="password"
-          changed={(event) => setPassword(event.target.value)}
-          value={password}
-        />
-        <Input
-          label="Confirm Password"
-          type="password"
-          changed={(event) => setConfirmPassword(event.target.value)}
-          value={confirmPassword}
-        />
-        <button type="submit">Register</button>
-        <span>
-          Already have an account? <Link>Login</Link>
-        </span>
-      </form>
+      {authForm}
     </div>
   );
 };
